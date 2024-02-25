aeac8ac1c4918ac0914fdbd2a262479f8081f9fc
Meditacion53 <alex_02_1992@hotmail.com>
2024-02-25 18:46:24 -0300

    subiendo mi primera pagina

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn') .addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+document.querySelector('.nav-menu') .addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+
